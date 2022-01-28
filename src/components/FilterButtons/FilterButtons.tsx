34f2ea3f42e048b99ddd0cdd1123d965a8f17c7a
manchikooo <manchiko.man31@mail.ru>
2022-01-28 14:56:50 +0300

    made filter of countries by select

--- src/components/FilterButtons/FilterButtons.tsx
+++ src/components/FilterButtons/FilterButtons.tsx
@@ -10,7 +10,7 @@ type RegionsFilterType = 'All' | 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oc
 
 export const FilterButtons = (props: FilterButtonsPropsType) => {
 
-    const [region, setRegion] = useState<RegionsFilterType>('All')
+    const [region, setRegion] = useState<string>('All')
 
     useEffect(() => {
         if (region === 'All') {
@@ -21,18 +21,26 @@ export const FilterButtons = (props: FilterButtonsPropsType) => {
         }
     }, [region])
 
-    const filterCountries = (region: RegionsFilterType) => {
-        setRegion(region)
+    const filterCountries = (e: string) => {
+        setRegion(e)
     }
 
     return (
         <div>
-            <button onClick={() => filterCountries('All')}>All</button>
-            <button onClick={() => filterCountries('Africa')}>Africa</button>
-            <button onClick={() => filterCountries('Americas')}>America</button>
-            <button onClick={() => filterCountries('Asia')}>Asia</button>
-            <button onClick={() => filterCountries('Europe')}>Europe</button>
-            <button onClick={() => filterCountries('Oceania')}>Oceania</button>
+            <select onChange={(e) => setRegion(e.currentTarget.value)}>
+                <option value='All'>All</option>
+                <option value='Africa'>Africa</option>
+                <option value='America'>America</option>
+                <option value='Asia'>Asia</option>
+                <option value='Europe'>Europe</option>
+                <option value='Oceania'>Oceania</option>
+            </select>
+            {/*<button onClick={() => filterCountries('All')}>All</button>*/}
+            {/*<button onClick={() => filterCountries('Africa')}>Africa</button>*/}
+            {/*<button onClick={() => filterCountries('Americas')}>America</button>*/}
+            {/*<button onClick={() => filterCountries('Asia')}>Asia</button>*/}
+            {/*<button onClick={() => filterCountries('Europe')}>Europe</button>*/}
+            {/*<button onClick={() => filterCountries('Oceania')}>Oceania</button>*/}
         </div>
     );
 };
